bf17a33e0e6c70d770254bc16e6488126ff89e52
niroshgowda <110534320+niroshgowda@users.noreply.github.com>
2023-10-06 15:06:23 +0530

    Delete Day 15 directory

--- Day 15/src/main/java/com/example/demo/repository/Adminrepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Admin;
-
-public interface Adminrepository extends JpaRepository<Admin, Integer> {
-
-}
